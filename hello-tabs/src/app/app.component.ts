f166636aa405c57de1133a979c4f51029fa30c91
Frédéric GAURAT <fgaurat@gmail.com>
2025-09-16 16:57:25 +0200

    end

--- hello-tabs/src/app/app.component.ts
+++ hello-tabs/src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Network } from '@capacitor/network';
+import { ToastController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+
+  private toastController: ToastController = inject(ToastController)
+
+
+  async ngOnInit(): Promise<void> {
+    Network.addListener('networkStatusChange', async (status) => {
+
+      const toast = await this.toastController.create({
+          message: `Network status changed ${status.connected}`,
+          duration: 1500,
+          position: 'bottom',
+        });
+
+        await toast.present();
+
+    });
+
+    const logCurrentNetworkStatus = async () => {
+      const status = await Network.getStatus();
+
+      console.log('Network status:', status);
+    };
+  }
+
+
 }
